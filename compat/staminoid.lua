39fa8aa3caf6203891b1051f209175cec71278d9
flux <25628292+fluxionary@users.noreply.github.com>
2023-04-19 17:16:13 -0700

    nil check

--- compat/staminoid.lua
+++ compat/staminoid.lua
@@ -2,7 +2,7 @@ staminoid.register_on_exhaust_player(function(player, amount, reason)
 	if type(reason) == "table" and reason.type == "dig" and reason.node then
 		local player_name = player:get_player_name()
 		local process = choppy.api.get_process(player_name)
-		if choppy.api.is_same_tree(process.tree_name, reason.node.name) then
+		if process and choppy.api.is_same_tree(process.tree_name, reason.node.name) then
 			return amount * 0.25
 		end
 	end
